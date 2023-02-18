f2760750db72ed2bfb0091a1f3ce2268dd297aa3
aishwary2994 <125033355+aishwary2994@users.noreply.github.com>
2023-02-18 13:58:13 +0530

    complete the sign in functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react'
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai"
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import OAuth from '../components/OAuth'
+import { signInWithEmailAndPassword, getAuth} from 'firebase/auth'
+import {toast} from "react-toastify"
+
+
+
 export default function SignIn() {
   const [showPassword, setShowPassword] = useState(false)
   const [formData, setFormData] = useState({
@@ -9,12 +14,26 @@ export default function SignIn() {
     password: ""
   })
   const{email, password} = formData
+  const navigate = useNavigate()
   function onChange(e){
     setFormData((prevState)=>({
       ...prevState,
       [e.target.id]:e.target.value,
     }))
   }
+  async function onSubmit(e){
+      e.preventDefault()
+      try {
+        const auth = getAuth()
+        const userCredentia = await signInWithEmailAndPassword(auth, email, password)
+        if(userCredentia.user){
+          navigate("/")
+        }
+      } catch (error) {
+        toast.error("Bad user credentials")
+      }
+  }
+
   return (
     <section>
       <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
@@ -23,7 +42,7 @@ export default function SignIn() {
           <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=773&q=80" alt="key" className="w-full rounded-2xl"/>
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onSubmit}>
             <input type="email" id="email" value={email} onChange={onChange} placeholder="Email address" className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mb-6"/>
             <div className="relative mb-6">
             <input type={showPassword ? "text" : "password"} id="password" value={password} onChange={onChange} placeholder="Password" className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"/>
