a84d22579daef86d59e12f24f79c3783a4ddc4b1
aishwary2994 <125033355+aishwary2994@users.noreply.github.com>
2023-02-25 22:02:20 +0530

    Create the landlord contact component

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -12,12 +12,16 @@ import SwiperCore, {
 } from "swiper"
 import "swiper/css/bundle"
 import {FaShare, FaMapMarkerAlt, FaBed, FaBath, FaParking, FaChair} from 'react-icons/fa'
+import  {getAuth} from 'firebase/auth'
+import Contact from '../components/Contact'
 
 export default function Listing() {
+  const auth = getAuth()
     const params = useParams()
     const[listing, setListing] = useState(null)
     const [loading, setLoading] = useState(true)
     const [shareLinkCopied, setShareLinkCopied] = useState(false)
+    const [contactLandlord, setContactLandlord] = useState(false)
     SwiperCore.use([Autoplay, Navigation, Pagination])
     useEffect(()=>{
         async function fetchListing(){
@@ -67,13 +71,14 @@ export default function Listing() {
         <FaShare className='text-lg text-slate-500'/>      
       </div>
       {shareLinkCopied && (
-        <p className='fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2'>Link Copied</p>
+        <p className='fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2'>Link Copied
+        </p>
       )}
 
-      <div className='m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white lg:space-x-5'>
-        <div className='w-full h-[200px] lg-[400px]z-10 overflow-x-hidden'>
+      <div className='m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white lg:space-x-5 '>
+        <div className='w-full'>
           <p className='text-2xl font-bold mb-3 text-blue-900'>
-            {listing.name} - ₹ {listing.offer ? listing.discountedPrice.toString()
+            {listing.name} - ₹{" "} {listing.offer ? listing.discountedPrice.toString()
                   .replace(/\B(?=(\d{3})+(?!\d))/g, ",") 
                   : listing.regularPrice.toString()
                   .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
@@ -113,6 +118,14 @@ export default function Listing() {
                   {listing.furnished ? "Furnished" : "Not furnished"}
                 </li>
               </ul>
+              {listing.userRef !== auth.currentUser?.uid && !contactLandlord && (
+              <div className='mt-6'>
+              <button onClick={()=>setContactLandlord(true)} className='px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg w-full text-center transition duration-150 ease-in-out'>Contact Landlord</button>
+              </div>
+              )}
+              {contactLandlord &&(
+                <Contact userRef={listing.userRef} listing={listing}/>
+              )}
             <div>
             </div>
         </div>
